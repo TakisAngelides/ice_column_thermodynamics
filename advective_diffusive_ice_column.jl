08d1960464e5d10fb0a185c92ea889edb24287f1
Takis Angelides <takis.angelides@gmail.com>
2026-01-07 17:58:16 +0100

    first commit

--- advective_diffusive_ice_column.jl
+++ advective_diffusive_ice_column.jl
@@ -35,7 +35,17 @@ n_lines = 10 # number of line snapshots
 # =========================
 ξ = LinRange(0, 1, n)
 ω = Pe .* ξ
-h = 1/(n-1)
+
+function setup_grid(spacing; spacing_order=2.0, spacing_factor=2.0)
+    spacing == "even" && return ξ
+    spacing == "polynomial" && return ξ .^ spacing_order
+    spacing == "exponential" &&
+        return (exp.(spacing_factor .* ξ) .- 1) ./ (exp(spacing_factor) - 1)
+    error("Unknown spacing")
+end
+
+ζ = setup_grid(spacing)
+h = diff(ζ)
 
 # =========================
 # Time evolution
@@ -54,21 +64,60 @@ function time_evolution()
     counter = 2
 
     # =========================
+    # Coefficients
+    # =========================
+    h_1, h_2, h_3, h_4 = h[1:end-3], h[2:end-2], h[3:end-1], h[4:end]
+    H_2 = h_1 .+ h_2 .+ h_3 .+ h_4
+
+    a_1 = @. (-2h_2*(2h_3+h_4) + 2h_3*(h_3+h_4)) /
+             (h_1*(h_1+h_2)*(h_1+h_2+h_3)*H_2)
+
+    a_2 = @. (2(h_1+h_2)*(2h_3+h_4) - 2h_3*(h_3+h_4)) /
+             (h_1*h_2*(h_1+h_3)*(h_2+h_3+h_4))
+
+    a_3 = @. (2h_2*(h_1+h_2) - 2(h_1+2h_2)*(2h_3+h_4) + 2h_3*(h_3+h_4)) /
+             ((h_1+h_2)*h_2*h_3*(h_3+h_4))
+
+    a_4 = @. (2(h_1+2h_2)*(h_3+h_4) - 2h_2*(h_1+h_2)) /
+             ((h_1+h_2+h_3)*(h_2+h_3)*h_3*h_4)
+
+    a_5 = @. (2(h_1+h_2)*h_2 - 2(h_1+2h_2)*h_3) /
+             (H_2*(h_2+h_3+h_4)*(h_3+h_4)*h_4)
+
+    b_1 = @. (2h[1] + h[2]) / (h[1]*(h[1] + h[2]))
+    b_2 = @. -(h[1] + h[2]) / (h[1]*h[2])
+    b_3 = @. h[1] / (h[2]*(h[1] + h[2]))
+
+    # =========================
     # Time loop
     # =========================
     for t in 1:tsteps
 
-        diffusion = @. (-θ_before[1:end-4] + 16*θ_before[2:end-3] -30*θ_before[3:end-2] + 16*θ_before[4:end-1] -θ_before[5:end]) / (12*h^2)
+        diffusion = @. a_1*θ_before[1:end-4] + a_2*θ_before[2:end-3] +
+                        a_3*θ_before[3:end-2] + a_4*θ_before[4:end-1] +
+                        a_5*θ_before[5:end]
 
-        advection = @. ω[3:end-2] * (θ_before[4:end-1] - θ_before[2:end-3]) / (2*h)
+        advection = @. ω[3:end-2] *
+                        (θ_before[4:end-1] - θ_before[2:end-3]) /
+                        (h_3 + h_2)
 
-        θ_now[3:end-2] = @. θ_before[3:end-2] + Δτ*(diffusion + advection + Ω)
+        θ_now[3:end-2] = @. θ_before[3:end-2] +
+                            Δτ*(diffusion + advection + Ω)
 
-        θ_now[2] = θ_before[2] + Δτ*(((θ_before[3] - 2*θ_before[2] + θ_before[1]) / h^2) + ω[2]*(θ_before[3]-θ_before[2])/(2*h) + Ω)
-        θ_now[end-1] = θ_before[end-1] + Δτ*(((θ_before[end] - 2*θ_before[end-1] + θ_before[end-2]) / h^2) + ω[end-1]*(θ_before[end]-θ_before[end-2])/(2*h) + Ω)
+        θ_now[2] = θ_before[2] + Δτ*(
+            2*(h_2[1]*θ_before[3] - (h_3[1]+h_2[1])*θ_before[2] + h_3[1]*θ_before[1]) /
+            (h_3[1]*h_2[1]*(h_3[1]+h_2[1])) +
+            ω[2]*(θ_before[3]-θ_before[2])/(h_3[1]+h_2[1]) + Ω
+        )
 
-        θ_now[1] = (2*h*γ + 4*θ_now[2] - θ_now[3]) / 3
-        θ_now[end] = (1 - (β/(2*h))*(θ_now[end-2] - 4*θ_now[end-1])) / (1 + (3*β)/(2*h))
+        θ_now[end-1] = θ_before[end-1] + Δτ*(
+            2*(h_2[end]*θ_before[end] - (h_3[end]+h_2[end])*θ_before[end-1] + h_3[end]*θ_before[end-2]) /
+            (h_3[end]*h_2[end]*(h_3[end]+h_2[end])) +
+            ω[end-1]*(θ_before[end]-θ_before[end-2])/(h_3[end]+h_2[end]) + Ω
+        )
+
+        θ_now[1] = (γ - b_2*θ_now[2] - b_3*θ_now[3]) / b_1
+        θ_now[end] = (β*θ_now[end-1] + h[end]) / (β + h[end])
 
         if mod(t, plot_every) == 0
             Θ[:, counter] .= θ_now
